714e302db3725708e007bf1a61cb46528837f735
ingarondel <inga404@icloud.com>
2024-05-18 09:01:22 -0700

    feat: sign up spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -9,14 +9,14 @@ feature 'User can sign in', %q{
 	given(:user) {create(:user)}
 	background { visit new_user_session_path }
 
-	scenario 'Registered user tries to sign up' do  
+	scenario 'Registered user tries to sign in' do  
 		fill_in 'Email', with: user.email
 		fill_in 'Password', with: user.password
 		click_on 'Log in'
 
 		expect(page).to have_content 'Signed in successfully.'
 	end
-	scenario 'Unregistered user tries to sign up' do 
+	scenario 'Unregistered user tries to sign in' do 
 		fill_in 'Email', with: 'us@user.com'
 		fill_in 'Password', with: '123456'
 		click_on 'Log in'

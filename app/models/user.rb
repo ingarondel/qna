c0109b7bc5fddbd072f48df0a84da550367c3186
ingarondel <inga404@icloud.com>
2024-05-18 09:24:52 -0700

    feat: Join tables answers and questions with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  has_many :questions
+  has_many :answers
+  
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def is_author?(resource)
+      self.id == resource.user_id
+    end
 end

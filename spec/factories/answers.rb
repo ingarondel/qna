f2f9a656017ddd6eb11b6a139e3667ba4d3e6fe1
ingarondel <inga404@icloud.com>
2024-05-18 04:29:12 -0700

    feat: add AnswersController with rspecs

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :answer do
     body { "MyText" }
-    correct { false }
+    correct { true }
+    association :question
+
+    trait :invalid do  
+      body { nil }
+    end
   end
 end

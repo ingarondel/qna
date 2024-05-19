4fc74a8b97c7240e1f7b31a44bdc348ab2487955
ingarondel <inga404@icloud.com>
2024-05-19 05:17:53 -0700

    feat: specs for update, delete answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -34,4 +34,114 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
   end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      before { login(user) }
+      it 'assigns the requested answer to @answer' do
+        patch :update, params: { id: answer, answer: attributes_for(:answer) }
+        expect(assigns(:answer)).to eq answer
+      end
+      it 'changes answer attributes' do
+        patch :update, params: { id: answer, answer: { body: 'body' } }
+        answer.reload
+
+        expect(answer.body).to eq 'body'
+      end
+      it 'redirects to question' do
+        patch :update, params: { id: answer, answer: attributes_for(:answer) }
+        expect(response).to redirect_to answer.question
+      end
+    end
+    context 'with invalid attributes' do
+      before do
+        login(user)
+        patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }
+      end
+      it 'does not change answer' do
+        answer.reload
+
+        expect(answer.body).to eq 'Answer'
+      end
+      it 're-renders question' do
+        expect(response).to render_template 'questions/show'
+      end
+    end
+
+    context 'for not the author of the answer' do
+      let(:not_author) { create(:user) }
+
+      before do
+        login(not_author)
+        patch :update, params: { id: answer, answer: { body: 'body' } }
+      end
+
+      it 'does not change answer' do
+        answer.reload
+
+        expect(answer.body).to eq 'Answer'
+      end
+
+      it 'redirects to question' do
+        expect(response).to redirect_to answer.question
+      end
+    end
+
+    context 'for unauthenticated user' do
+      before { patch :update, params: { id: answer, answer: { body: 'body' } } }
+
+      it 'does not change answer' do
+        answer.reload
+
+        expect(answer.body).to eq 'Answer'
+      end
+
+      it 'redirects to sign up page' do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+   describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question, user: user) }
+
+    context 'for the author of the answer' do
+      before { login(user) }
+
+      it 'deletes the answer' do
+        expect { delete :destroy, params: { id: answer } }.to change(question.answers, :count).by(-1)
+      end
+
+      it 'redirects to question' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to answer.question
+      end
+    end
+
+    context 'for not the author of the answer' do
+      let(:not_author) { create(:user) }
+
+      before { login(not_author) }
+
+      it "don't delete the answer" do
+        expect { delete :destroy, params: { id: answer } }.to_not change(Answer, :count)
+      end
+
+      it 'redirects to question' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to answer.question
+      end
+    end
+
+    context 'for unauthenticated user' do
+      it "don't delete the answer" do
+        expect { delete :destroy, params: { id: answer } }.to_not change(question.answers, :count)
+      end
+
+      it 'redirects to sign up page' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
